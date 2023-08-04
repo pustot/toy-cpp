1fe7f95ad8b9d22033a06dbee945f7989bdfa494
朵牛 TwaqNgu <i@yangchnx.com>
2023-08-05 03:16:58 +0800

    add 1 猩猩派普化石屏话模拟

--- main.cpp
+++ main.cpp
@@ -1,20 +1,37 @@
-﻿// toy-cpp.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <string>
+#include <unordered_map>
 
 int main()
 {
     std::cout << "Hello World! 来自朵牛" <<std::endl;
+    std::cout << "请选择要进入的功能（按回车结束）：" << std::endl;
+    std::cout << "1 猩猩派普化石屏话模拟" << std::endl;
+    int selectedOption = 0;
+    std::cin >> selectedOption;
+    if (selectedOption == 1) {
+        std::string py;
+        std::cout << "输入拼音（声调1234，支持轻声0）（示例：tou2 fa0）（按回车结束）：" << std::endl;
+        getline(std::cin, py);
+        getline(std::cin, py);
+        std::string shiping;
+        std::unordered_map<std::string, std::string> toneM = {
+            {"1", "55"}, {"2", "42"}, {"3", "22"}, {"4", "21"}, {"0", "55"}
+        };
+        std::unordered_map<std::string, std::string> pronM = {
+            {"ou", "ei"}, {"u", "i"}
+        };
+        for (int i = 0; i < py.size(); i++) {
+            if (toneM.count(py.substr(i, 1)))
+                shiping += toneM[py.substr(i, 1)];
+            else if (i < py.size() - 1 && pronM.count(py.substr(i, 2))) {
+                shiping += pronM[py.substr(i, 2)];
+                i++;
+            } else if (i < py.size() - 1 && pronM.count(py.substr(i, 1)))
+                shiping += pronM[py.substr(i, 1)];
+            else
+                shiping += py[i];
+        }
+        std::cout << shiping << std::endl;
+    }
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
